e5ccabcfba6ad0694986c1cdf7da788eaee37e51
cExcalibur <939187596@qq.com>
2017-07-18 10:15:52 +0800

    task

--- B0/Student.rb
+++ B0/Student.rb
@@ -7,14 +7,8 @@ class Student
   def look
     puts"id：#@id,姓名：#@name,性别：#@gender,年龄：#@age"
   end
-  def write
-    aFile=File.new("Student.yaml","a")
-    aFile.syswrite("
-    - id: #@id\n
-      name: #@name\n
-      gender: #@gender\n
-      age: #@age\n")
-    aFile.close
+  def to_s
+    "id：#@id,姓名：#@name,性别：#@gender,年龄：#@age"
   end
 end
 
@@ -25,14 +19,13 @@ def newname(len)
   return newname
 end
 
-aFile=File.new("Student.yaml","a")
-aFile.syswrite("students:\n")
-aFile.close
-$num=1
+num=1
+students=Array.new
+
 100.times{
   student=Student.new()
   student.name=newname(6)
-  student.id=$num
+  student.id=num
   if(rand(2)==1)
     student.gender="男"
   else
@@ -40,10 +33,11 @@ $num=1
   end
   student.age=(rand(6)+15)
   student.look
-  student.write
-  $num+=1
+  students<<student
+  num+=1
 }
 
+File.open("Student.yaml","wb"){|f| YAML.dump(students,f)}
 puts"信息存入成功"
 problem = YAML.load(File.open("Student.yaml"))
-problem["students"].each{ |c| puts"#{c}\n"}
+problem.each{|c| puts"#{c}"}
